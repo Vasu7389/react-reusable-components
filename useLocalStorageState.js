01eedd50af37676df7f5cfa0ae064b19591ae059
Vasu7389 <awasthivasu.65@gmail.com>
2021-07-18 15:39:42 +0530

    Local storage react hook

---
+++ useLocalStorageState.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+/**
+ *
+ * @param {String} key The key to set in localStorage
+ * @param {Object} defaultValue The value to use if it is not already in localStorage
+ */
+function useLocalStorageState(key, defaultValue = "") {
+  const [state, setState] = useState(() => 
+     JSON.parse(window.localStorage.getItem(key)) || defaultValue;
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+}
+
+export { useLocalStorageState };
