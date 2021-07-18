9d5ee24c3bfcddaabb6533c95d1cd5cf02395831
Vasu7389 <awasthivasu.65@gmail.com>
2021-07-18 15:50:45 +0530

    Local storage react hook - bug fix

--- useLocalStorageState.js
+++ useLocalStorageState.js
@@ -6,8 +6,8 @@ import React, { useState, useEffect } from "react";
  * @param {Object} defaultValue The value to use if it is not already in localStorage
  */
 function useLocalStorageState(key, defaultValue = "") {
-  const [state, setState] = useState(() => 
-     JSON.parse(window.localStorage.getItem(key)) || defaultValue;
+  const [state, setState] = useState(
+    () => JSON.parse(window.localStorage.getItem(key)) || defaultValue
   );
 
   useEffect(() => {
